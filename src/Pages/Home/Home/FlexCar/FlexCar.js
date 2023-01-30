9b19d32e3931b2372c9c5b3ad72bb47f69c0a454
Mahinkahn <mahinkahn37@gmail.com>
2023-01-30 18:48:59 +0600

    mail add icon

--- src/Pages/Home/Home/FlexCar/FlexCar.js
+++ src/Pages/Home/Home/FlexCar/FlexCar.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import mail from '../../../../assets/images/mail.png'
 
 const FlexCar = () => {
     return (
         <div className="hero h-[323px] bg-primary rounded-md mt-[150px]">
-            <div className="hero-content flex-col lg:flex-row-reverse gap-96">
-                <button className="btn gap-2 hover:scale-110">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
-                    Get in touch now
-                </button>
-                <div className='mr-44'>
-                    <h1 className="text-5xl font-bold text-white leading-normal">Box Office News <br />Office News</h1>
+            <div className="hero-content flex-col lg:flex-row gap-96 mr-[200px]">
+                <h1 className="text-5xl font-bold text-white leading-normal">Ready to launch  <br />your next website?</h1>
+                <div className='ml-[100px] hero-content flex-col lg:flex-col'>
+                    <button className="btn gap-3 h-14 w-[245px] text-accent font-bold rounded-full hover:scale-110">
+                        <img src={mail} alt="" />
+                        Get in touch now
+                    </button>
+                    <p className='text-white mr-6'>Or, take a peek inside our design studio</p>
                 </div>
             </div>
         </div>
